0489534ee081506629bd95af53850e7d69f14a12
CarlOlson <CarlOlson@users.noreply.github.com>
2018-05-03 22:25:51 -0500

    add more cases to plspec_spec

--- plspec_spec.pl
+++ plspec_spec.pl
@@ -1,8 +1,18 @@
 
+failure_with_stack :-
+    !,
+    1 == 0.
+
 :- describe(describe/1).
 
 it('should be able to test itself') :-
     A is 2,
     A = 2.
+
+it('should fail and print a stack') :-
+    failure_with_stack.
+
+it('should fail and print an error') :-
+    prefixr("yolo", _).
 
 :- end(describe/1).
